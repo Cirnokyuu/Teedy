af88cd2843702ba4679773b48991ff99bb3d9051
cirnokyuu <xzhiongiao@qq.com>
2025-05-19 00:39:04 +0800

    register

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,58 +1,55 @@
 pipeline { 
-    agent any 
-    environment { 
-// define environment variable 
-// Jenkins credentials configuration 
-        DOCKER_HUB_CREDENTIALS = credentials('dockerhub_credentials') // Docker 
-Hub credentials ID store in Jenkins 
-// Docker Hub Repository's name 
-DOCKER_IMAGE = 'miko3535/teedy-app'
-        DOCKER_TAG = "${env.BUILD_NUMBER}" // use build number as tag 
-    } 
-    stages { 
-        stage('Build') { 
+   agent any 
+   environment { 
+      DOCKER_HUB_CREDENTIALS = credentials('dockerhub_credentials')
+      DOCKER_IMAGE = 'miko3535/teedy-app'
+      DOCKER_TAG = "${env.BUILD_NUMBER}" // use build number as tag 
+   } 
+   stages {
+      stage('Build') { 
             steps { 
                checkout scmGit( 
                   branches: [[name: '*/master']],  
                   extensions: [],  
                   userRemoteConfigs: [[url: 'https://github.com/Cirnokyuu/Teedy.git']] 
-                ) 
-                sh 'mvn -B -DskipTests clean package' 
+               ) 
+               sh 'mvn -B -DskipTests clean package' 
             } 
-        } 
-        stage('Building image') { 
-            steps { 
-                script { 
-                    docker.build("${env.DOCKER_IMAGE}:${env.DOCKER_TAG}") 
-                } 
-            } 
-        } 
-        stage('Upload image') { 
-            steps { 
-                script { 
-                  docker.withRegistry('https://registry.hub.docker.com', 'DOCKER_HUB_CREDENTIALS') { 
-                  // push image 
+      } 
+      stage('Building image') { 
+         steps { 
+               script { 
+                  docker.build("${env.DOCKER_IMAGE}:${env.DOCKER_TAG}") 
+               } 
+         } 
+      } 
+      stage('Upload image') { 
+         steps { 
+            script { 
+               docker.withRegistry('https://registry.hub.docker.com', 'dockerhub_credentials') { 
+               // push image 
                   docker.image("${env.DOCKER_IMAGE}:${env.DOCKER_TAG}").push() 
-                  // ：optional: label latest 
-                  // docker.image("${env.DOCKER_IMAGE}:${env.DOCKER_TAG}").push('latest') 
-                  } 
-                } 
-            }  } 
+               // ：optional: label latest 
+               // docker.image("${env.DOCKER_IMAGE}:${env.DOCKER_TAG}").push('latest') 
+               }
+            } 
+         }
+      } 
 // Running Docker container 
-        stage('Run containers') { 
-            steps { 
-                script { 
-// stop then remove containers if exists 
-sh 'docker stop teedy-container-8081 || true' 
-sh 'docker rm teedy-container-8081 || true' 
-// run Container 
-docker.image("${env.DOCKER_IMAGE}:${env.DOCKER_TAG}").run( 
-'--name teedy-container-8081 -d -p 8081:8080' 
-) 
-// Optional: list all teedy-containers 
-sh 'docker ps --filter "name=teedy-container"' 
-} 
-} 
-        } 
-    } 
+      stage('Run containers') {
+         steps { 
+            script { 
+               // stop then remove containers if exists 
+               sh 'docker stop teedy-container-8081 || true' 
+               sh 'docker rm teedy-container-8081 || true' 
+               // run Container 
+               docker.image("${env.DOCKER_IMAGE}:${env.DOCKER_TAG}").run( 
+               '--name teedy-container-8081 -d -p 8081:8080' 
+               ) 
+               // Optional: list all teedy-containers 
+               sh 'docker ps --filter "name=teedy-container"' 
+            }
+         }
+      } 
+   } 
 } 
--- docs-web/src/main/webapp/src/app/docs/controller/Login.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Login.js
@@ -3,39 +3,39 @@
 /**
  * Login controller.
  */
-angular.module('docs').controller('Login', function(Restangular, $scope, $rootScope, $state, $stateParams, $dialog, User, $translate, $uibModal) {
+angular.module('docs').controller('Login', function (Restangular, $scope, $rootScope, $state, $stateParams, $dialog, User, $translate, $uibModal) {
   $scope.codeRequired = false;
 
   // Get the app configuration
-  Restangular.one('app').get().then(function(data) {
+  Restangular.one('app').get().then(function (data) {
     $rootScope.app = data;
   });
 
   // Login as guest
-  $scope.loginAsGuest = function() {
+  $scope.loginAsGuest = function () {
     $scope.user = {
       username: 'guest',
       password: ''
     };
     $scope.login();
   };
-  
+
   // Login
-  $scope.login = function() {
-    User.login($scope.user).then(function() {
-      User.userInfo(true).then(function(data) {
+  $scope.login = function () {
+    User.login($scope.user).then(function () {
+      User.userInfo(true).then(function (data) {
         $rootScope.userInfo = data;
       });
 
-      if($stateParams.redirectState !== undefined && $stateParams.redirectParams !== undefined) {
+      if ($stateParams.redirectState !== undefined && $stateParams.redirectParams !== undefined) {
         $state.go($stateParams.redirectState, JSON.parse($stateParams.redirectParams))
-          .catch(function() {
+          .catch(function () {
             $state.go('document.default');
           });
       } else {
         $state.go('document.default');
       }
-    }, function(data) {
+    }, function (data) {
       if (data.data.type === 'ValidationCodeRequired') {
         // A TOTP validation code is required to login
         $scope.codeRequired = true;
@@ -43,9 +43,77 @@ angular.module('docs').controller('Login', function(Restangular, $scope, $rootSc
         // Login truly failed
         var title = $translate.instant('login.login_failed_title');
         var msg = $translate.instant('login.login_failed_message');
-        var btns = [{result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary'}];
+        var btns = [{ result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary' }];
         $dialog.messageBox(title, msg, btns);
       }
+    });
+  };
+
+  // 注册弹窗
+  $scope.register = function () {
+    $uibModal.open({
+      template:
+        '<div class="modal-header">' +
+        '  <h3>{{ "login.register" | translate }}</h3>' +
+        '</div>' +
+        '<div class="modal-body">' +
+        '  <form name="registerForm" novalidate autocomplete="off">' +
+        '    <div class="form-group">' +
+        '      <label>{{ "settings.user.edit.username" | translate }}</label>' +
+        '      <input class="form-control" type="text" name="username" ng-model="user.username" required ng-pattern="/^[a-zA-Z0-9_@.-]*$/" ng-minlength="3" ng-maxlength="50" ng-attr-placeholder="{{ \'settings.user.edit.username\' | translate }}" />' +
+        '    </div>' +
+        '    <div class="form-group">' +
+        '      <label>{{ "settings.user.edit.email" | translate }}</label>' +
+        '      <input class="form-control" type="email" name="email" ng-model="user.email" required ng-minlength="1" ng-maxlength="100" ng-attr-placeholder="{{ \'settings.user.edit.email\' | translate }}" />' +
+        '    </div>' +
+        '    <div class="form-group">' +
+        '      <label>{{ "settings.user.edit.password" | translate }}</label>' +
+        '      <input class="form-control" type="password" name="password" ng-model="user.password" required ng-minlength="8" ng-maxlength="50" ng-attr-placeholder="{{ \'settings.user.edit.password\' | translate }}" />' +
+        '    </div>' +
+        '    <div class="form-group">' +
+        '      <label>{{ "settings.user.edit.password_confirm" | translate }}</label>' +
+        '      <input class="form-control" type="password" name="passwordconfirm" ng-model="user.passwordconfirm" required ng-attr-placeholder="{{ \'settings.user.edit.password_confirm\' | translate }}" />' +
+        '    </div>' +
+        '  </form>' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '  <button class="btn btn-success" ng-click="ok()" ng-disabled="registerForm.$invalid">{{ "login.register" | translate }}</button>' +
+        '  <button class="btn btn-default" ng-click="cancel()">{{ "cancel" | translate }}</button>' +
+        '</div>',
+      controller: function ($scope, $uibModalInstance, Restangular, $dialog, $translate) {
+        $scope.user = {};
+
+        $scope.ok = function () {
+          if ($scope.user.password !== $scope.user.passwordconfirm) {
+            var title = $translate.instant('validation.password_confirm');
+            var msg = $translate.instant('validation.password_confirm');
+            var btns = [{ result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary' }];
+            $dialog.messageBox(title, msg, btns);
+            return;
+          }
+          var user = angular.copy($scope.user);
+          user.storage_quota = 10000 * 1000000;
+          user.disabled = true;
+          Restangular.one('user').put(user).then(function () {
+            var title = $translate.instant('login.register');
+            var msg = '注册申请成功，请等待管理员审核';
+            var btns = [{ result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary' }];
+            $dialog.messageBox(title, msg, btns, function () {
+              $uibModalInstance.close();
+            });
+          }, function (e) {
+            var title = $translate.instant('login.register');
+            var msg = e.data && e.data.message ? e.data.message : $translate.instant('settings.user.edit.edit_user_failed_message');
+            var btns = [{ result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary' }];
+            $dialog.messageBox(title, msg, btns);
+          });
+        };
+
+        $scope.cancel = function () {
+          $uibModalInstance.dismiss('cancel');
+        };
+      },
+      size: 'md'
     });
   };
 
@@ -65,12 +133,12 @@ angular.module('docs').controller('Login', function(Restangular, $scope, $rootSc
       }).then(function () {
         var title = $translate.instant('login.password_lost_sent_title');
         var msg = $translate.instant('login.password_lost_sent_message', { username: username });
-        var btns = [{result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary'}];
+        var btns = [{ result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary' }];
         $dialog.messageBox(title, msg, btns);
       }, function () {
         var title = $translate.instant('login.password_lost_error_title');
         var msg = $translate.instant('login.password_lost_error_message');
-        var btns = [{result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary'}];
+        var btns = [{ result: 'ok', label: $translate.instant('ok'), cssClass: 'btn-primary' }];
         $dialog.messageBox(title, msg, btns);
       });
     });
